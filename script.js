3f24d86926b19ee5836f3df484dfbbb85b775b7f
Hexer <40330756+shadowhexer@users.noreply.github.com>
2024-10-17 12:21:26 +0800

    Update script.js

--- script.js
+++ script.js
@@ -23,16 +23,36 @@ function showSlide(index) {
 }
 
 // Show the next slide
-function nextSlide() {
-    currentSlide = (currentSlide + 1) % slides.length;
-    showSlide(currentSlide);
+const slides = document.querySelectorAll('.slide');
+let index = 0;
+
+function prevSlide(){
+    slides[index].classList.remove('active');
+    index--;
+
+    if(index < 0)
+        index = slides.length -1;
+
+    slides[index].classList.add('active');      
 }
 
-// Show the previous slide
-function prevSlide() {
-    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
-    showSlide(currentSlide);
+document.querySelector('.prev').addEventListener('click', e => {
+    prevSlide();
+});
+
+function nextSlide(){
+    slides[index].classList.remove('active');
+    index++;
+
+    if(index > slides.length -1)
+        index = 0;
+
+    slides[index].classList.add('active');      
 }
+
+document.querySelector('.next').addEventListener('click', e => {
+    nextSlide();
+});
 
 // Auto-slide every 5 seconds
 setInterval(nextSlide, 5000);
